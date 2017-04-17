43d111f460514a682afeba882ce0d6b67d83775e
VARUN\varun <varunjn591@gmail.com>
2017-04-17 02:26:18 -0500

    adding updates

--- src/main/java/com/target/controller/ProductController.java
+++ src/main/java/com/target/controller/ProductController.java
@@ -1,5 +1,18 @@
 package com.target.controller;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Path("/Product")
 public class ProductController {
 
+	@GET
+	@Path("text")
+	public Response getText(){
+		return Response.status(200).entity("Hello").build();
+	}
 }
