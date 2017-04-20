de230f7343d48ca1a33af439d096d576ca66fef9
VARUN\varun <varunjn591@gmail.com>
2017-04-20 02:11:35 -0500

    Adding new files

--- src/main/java/com/target/service/product/ProductServiceImpl.java
+++ src/main/java/com/target/service/product/ProductServiceImpl.java
@@ -14,30 +14,15 @@ public class ProductServiceImpl implements ProductService {
 
 	private Log logger;
 
+	/*
+	 * check status
+	 * check connection timeout
+	 * check socket timeout
+	 * 
+	 */
 	@Override
 	public JSONObject getProductServiceResponse() {
 		 String productDescriptionApiUrl = "http://redsky.target.com/v1/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
-		//String productDescriptionApiUrl = "http://echo.jsontest.com/insert-key-here/insert-value-here/key/value";
-		// creating request
-		/*
-		 * Client client = ClientBuilder.newClient(); StringBuffer
-		 * productDescUrl = new StringBuffer();
-		 * productDescUrl.append(productDescriptionApiUrl); WebTarget target =
-		 * client.target(productDescriptionApiUrl);
-		 * 
-		 * JSONObject productDescResponse = null; try{ Invocation.Builder
-		 * invocationBuilder = target.request(MediaType.APPLICATION_JSON);
-		 * Response resp = invocationBuilder.get();
-		 * 
-		 * if(resp.getStatus() != 200){
-		 * logger.error("Could not connect/Unauthorized"); }
-		 * 
-		 * String result = resp.getEntity().toString(); if(result != null){
-		 * productDescResponse = new JSONObject(result); }else{
-		 * logger.error("Error no data returned"); }
-		 * 
-		 * } catch(JSONException j){ logger.error("Error parsing JSON"); }
-		 */
 		JSONObject productDescResponse = null;
 		String responseBody = null;
 		ResponseEntity<String> response = null;
